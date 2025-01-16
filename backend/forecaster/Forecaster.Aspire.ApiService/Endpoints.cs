ac2d1f724f4e323d607b5b4498def4837be7b8f8
Michal Wcislo <wcislom@gmail.com>
2025-01-16 07:45:07 +0100

    Moves Forecaster GET endpoint out of Program.cs

---
+++ backend/forecaster/Forecaster.Aspire.ApiService/Endpoints.cs
@@ -0,0 +1,30 @@
+﻿using Forecaster.Core;
+using System.Reflection.Metadata.Ecma335;
+
+namespace Forecaster.ApiService
+{
+    public static class Endpoints
+    {
+        public static void MapForecasterEndpoints(this WebApplication app)
+        {
+            app.MapGet("/weatherforecast", () =>
+            {
+                string[] summaries = ["Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"];
+
+                var forecast = Enumerable.Range(1, 5).Select(index =>
+                    new WeatherForecast
+                    (
+                        index,
+                        DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                        Random.Shared.Next(-20, 55),
+                        summaries[Random.Shared.Next(summaries.Length)]
+                    ))
+                    .ToArray();
+                return forecast;
+            })
+.WithName("GetWeatherForecast");
+        }
+
+        
+    }
+}
--- backend/forecaster/Forecaster.Aspire.ApiService/Program.cs
+++ backend/forecaster/Forecaster.Aspire.ApiService/Program.cs
@@ -1,3 +1,4 @@
+using Forecaster.ApiService;
 using Forecaster.Core;
 using Forecaster.Infrastructure.Database;
 
@@ -24,22 +25,8 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();
 }
 
-string[] summaries = ["Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"];
+app.MapForecasterEndpoints();
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            index,
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
 
 app.MapDefaultEndpoints();
 
