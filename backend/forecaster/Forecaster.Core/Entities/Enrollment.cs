c6ef622c9d810c69fb82a707eadc9fff4b1ebe9f
Michal Wcislo <wcislom@gmail.com>
2025-01-26 10:32:06 +0100

    Adds new entities to Core

--- backend/forecaster/Forecaster.Core/Entities/Enrollment.cs
+++ backend/forecaster/Forecaster.Core/Entities/Enrollment.cs
@@ -8,5 +8,16 @@ namespace Forecaster.Core.Entities
 {
     public class Enrollment
     {
+        public int Id { get; set; }
+        public int StudentId { get; set; }
+        public int CourseId { get; set; }
+        public Grade? Grade { get; set; }
+        public Student Student { get; set; }
+        public Course Course { get; set; }
+    }
+
+    public enum Grade
+    {
+        A, B, C, D, F
     }
 }
