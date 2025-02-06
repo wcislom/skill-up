9f9afff9364b8412aab7b5ea49a19a562e356064
Michal Wcislo <wcislom@gmail.com>
2025-02-06 08:23:13 +0100

    Beggining of using IMeterFactory for creating Meter

---
+++ backend/forecaster/Forecaster.Infrastructure/Metrics/CallsMeter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Forecaster.Infrastructure.Metrics
+{
+    internal class CallsMeter
+    {
+        public CallsMeter(IMeterFactory factory)
+        {
+            
+        }
+    }
+}
