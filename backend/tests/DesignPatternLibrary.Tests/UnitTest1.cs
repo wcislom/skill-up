8d1d285b274b6b6a0a1905c250435f24664ee617
Michal Wcislo <wcislom@gmail.com>
2025-02-12 07:46:56 +0100

    Adds Unit Tests

--- backend/tests/DesignPatternLibrary.Tests/UnitTest1.cs
+++ backend/tests/DesignPatternLibrary.Tests/UnitTest1.cs
@@ -1,11 +1,22 @@
-﻿namespace DesignPatternLibrary.Tests
+﻿using DesignPatternsLibrary.Decorator;
+
+namespace DesignPatternLibrary.Tests
 {
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public async void Decorator_Example()
         {
+            // Arrange
+            var decorated = new Component();
+            var decoratorA = new DecoratorA(decorated);
+            var decoratorB = new DecoratorB(decoratorA);
 
+            // Act
+            var result = await decoratorB.Operation();
+
+            // Assert
+            Assert.Equal("<DecoratorB><DecoratorA><Component>This comes from component</Component></DecoratorA></DecoratorB>", result);
         }
     }
 }
