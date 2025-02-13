a60506b1d8abd9ef69cebc4a39747d578623af59
Michal Wcislo <wcislom@gmail.com>
2025-02-13 07:33:12 +0100

    Adds Adapter example test

---
+++ backend/tests/DesignPatternLibrary.Tests/FacadeTests.cs
@@ -0,0 +1,22 @@
+﻿using DesignPatternsLibrary.Adapter;
+
+namespace DesignPatternLibrary.Tests
+{
+
+    public class FacadeTests
+    {
+        [Fact]
+        public async Task Adapter_Test()
+        {
+            // Arrange
+            var library = new ExternalOcrLib();
+            var adapter = new ExternalOcrLibAdapter(library);
+
+            // Act
+            var result = await adapter.Read(new MemoryStream([1, 2, 3]));
+
+            // Assert
+            Assert.Equal("This is the text extracted from the image", result);
+        }
+    }
+}
