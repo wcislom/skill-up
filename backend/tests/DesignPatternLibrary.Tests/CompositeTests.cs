7c3a421b81f6076e25ab9f24afa1ed520fcc948b
Michal Wcislo <wcislom@gmail.com>
2025-02-14 20:37:35 +0100

    Adds beginning of Composite Tests

---
+++ backend/tests/DesignPatternLibrary.Tests/CompositeTests.cs
@@ -0,0 +1,11 @@
+﻿namespace DesignPatternLibrary.Tests
+{
+    public class CompositeTests
+    {
+        [Fact]
+        public Task Composite_Test()
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
