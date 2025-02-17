30ed5e5a8edd0adc2ea7a5f0d1beb5d420d07f9a
Michal Wcislo <wcislom@gmail.com>
2025-02-17 07:52:07 +0100

    Adds beginning Template Method implementation

---
+++ backend/other/DesignPatternsLibrary/TemplateMethod/FileProcessor.cs
@@ -0,0 +1,27 @@
+﻿namespace DesignPatternsLibrary.TemplateMethod
+{
+    public abstract class FileProcessor
+    {
+        protected abstract Task<Stream> OpenFile(string path);
+
+        protected abstract Task<bool> Validate();
+
+        protected abstract Task ProcessInternal();
+
+        public async Task Process(string path)
+        {
+            var file = await OpenFile(path);
+            if(file == null)
+            {
+                throw new FileNotFoundException("File not found");
+            }
+
+            if (!await Validate())
+            {
+                throw new InvalidOperationException("File is not valid");
+            }
+
+            await ProcessInternal();
+        }
+    }
+}
