ba1e85ac1ca6ba945f8f2b06c8736a820d14bca2
Michal Wcislo <wcislom@gmail.com>
2025-02-21 13:05:49 +0100

    Adds another example of OperationResult

---
+++ backend/other/DesignPatternsLibrary/OperationResult/OperationResult.cs
@@ -0,0 +1,24 @@
+﻿namespace DesignPatternsLibrary.OperationResult
+{
+    public record OperationResult<T>
+    {
+        public bool IsSuccess { get; private set; }
+
+        public T? Value { get; private set; }
+
+        private OperationResult()
+        {
+        }
+
+        public OperationResult<T> Success(T value)
+        {
+            return new OperationResult<T> { Value = value, IsSuccess = true };
+        }
+
+        public OperationResult<T> Fail()
+        {
+            return new OperationResult<T> { Value = default, IsSuccess = false };
+        }
+
+    }
+}
