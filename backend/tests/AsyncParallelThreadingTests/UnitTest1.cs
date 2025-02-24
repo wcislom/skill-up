10b9672825f97064def200e7b3b78139ccb6d8ba
Michal Wcislo <wcislom@gmail.com>
2025-02-24 07:56:35 +0100

    Introduction to Advanced topics

---
+++ backend/tests/AsyncParallelThreadingTests/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using System.Diagnostics;
+
+namespace AsyncParallelThreadingTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Debug.WriteLine("Thread Id: {0}", Thread.CurrentThread.ManagedThreadId);
+            Debug.WriteLine("Threads in Thread Pool {0}", ThreadPool.ThreadCount);
+
+        }
+    }
+}
