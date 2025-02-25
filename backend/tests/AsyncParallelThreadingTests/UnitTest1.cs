6492052025841521a0ef3aa09a16bd322be782b5
Michal Wcislo <wcislom@gmail.com>
2025-02-25 08:17:29 +0100

    Playing with Task and CancellationToken

--- backend/tests/AsyncParallelThreadingTests/UnitTest1.cs
+++ backend/tests/AsyncParallelThreadingTests/UnitTest1.cs
@@ -5,11 +5,22 @@ namespace AsyncParallelThreadingTests
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public async void Test1()
         {
             Debug.WriteLine("Thread Id: {0}", Thread.CurrentThread.ManagedThreadId);
             Debug.WriteLine("Threads in Thread Pool {0}", ThreadPool.ThreadCount);
 
+            using var tokenSource = new CancellationTokenSource();
+            var token = tokenSource.Token;
+            var task = Task.Run(() =>
+            {
+                while (true)
+                {
+                    token.ThrowIfCancellationRequested();
+                }
+            }, token);
+            await Task.Delay(5000);
+            Debug.WriteLine("Task status is {0}", task.Status);
         }
     }
 }
